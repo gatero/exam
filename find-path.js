6aeb7984b1d484a860e0df080c3e58d65caed217
Daniel Ortega <contact@daniel-ortega.mx>
2019-11-24 15:48:34 -0600

    [answer]: I added solution proposal for findPath function

--- find-path.js
+++ find-path.js
@@ -7,5 +7,45 @@
     But if not found i should see 'not found'
 */
 export default function findPath(object, value) {
-  return '';
+  let path;
+  let found;
+
+ 	const findValue = (object, value) => {
+  	for (const key in object) {
+    	const currentValue = object[key];
+    	const isObject = typeof currentValue === 'object';
+      const isArray = Array.isArray(currentValue);
+    	const isEqual = isObject && isArray
+                      ? JSON.stringify(currentValue) === JSON.stringify(value)
+                      : currentValue === value;
+      if (!path) {
+        path = key;
+      } else {
+      	if (isObject || isEqual) {
+        	path += isNaN(key) ? '.' + key : '[' + key + ']';
+        }
+      }
+
+      if (isEqual) {
+      	found = true;
+      	break;
+      }
+
+      if (isObject) {
+      	findValue(currentValue, value);
+      }
+    }
+  };
+
+  findValue(object, value);
+
+  let chunks = path.split('.');
+
+  while (chunks.length > 2) {
+  	chunks.shift();
+  }
+
+  path = chunks.join('.');
+
+  return found ? path : 'not found';
 }
