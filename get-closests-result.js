c3e5331552476f833693be8fd48a016d7e67d4a2
Daniel Ortega <contact@daniel-ortega.mx>
2019-11-24 15:48:50 -0600

    [answer]: I added solution proposal for getClosestResult function

--- get-closests-result.js
+++ get-closests-result.js
@@ -11,5 +11,45 @@
     Example:
 */
 export default function getClosestResult(arr1, arr2, value) {
+  let num1, num2;
+  let memory = [];
+  let results = [];
+  let result, found;
+
+  arr1.forEach((n1) => arr2.forEach((n2) => (
+    memory.push((n1 + n2) + ':' + n1 + ':' + n2)
+    && results.push(n1 + n2)
+  )));
+
+  results = results.sort((a, b) => a - b);
+  result = results.find((result) => result === value);
+
+  if (!result) {
+    for (let num = value; num >= 0; num--) {
+      found = results.find((current) => current === num);
+      if (found) {
+        result = num;
+        break;
+      }
+    }
+    if (!result) {
+      for (let num = 0; num <= results.length; num++) {
+        found = results.find((current) => current === num);
+        if (found) {
+          result = num;
+          break;
+        }
+      }
+    }
+  }
+
+  if (result) {
+    const regexp = result ? new RegExp(`^${result}:`) : '';
+    const data = memory.find((slot) => regexp.test(slot)).split(':');
+
+    num1 = data[1];
+    num2 = data[2];
+  }
+
   return num1 +' + '+ num2 + ' is the closest';
 }
