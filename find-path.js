e847c17778ee3e5a3322ec15a1990e72054c7c43
Daniel Ortega <contact@daniel-ortega.mx>
2019-11-24 15:36:50 -0600

    [exercise]: add the initial state of findPath function

---
+++ find-path.js
@@ -0,0 +1,11 @@
+/*
+    Find the Path
+
+    Given an object which could be either Object or Array
+    When i provide a value to test
+    Then if found i should get the path to the value
+    But if not found i should see 'not found'
+*/
+export default function findPath(object, value) {
+  return '';
+}
