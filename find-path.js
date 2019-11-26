47d644b7f4fb01ad3db1e6869a6f238aaab82049
Daniel Ortega <contact@daniel-ortega.mx>
2019-11-26 00:52:02 -0600

    [exercise]: resolve structural problems

--- find-path.js
+++ find-path.js
@@ -7,45 +7,49 @@
     But if not found i should see 'not found'
 */
 export default function findPath(object, value) {
-  let path;
-  let found;
+  const memory = [];
+  const getType = (object) => Array.isArray(object) ? 'array' : typeof object;
+  const valueType = getType(value);
+  const handler = {
+    object: (object, path) => Object.keys(object).map((property, index) => {
+      const objectType = getType(object[property]);
+      const slot = {
+        path: (() => {
+          console.log('path: ', path, 'property: ', property);
+          if (path) {
+            return isNaN(property) ? path + '.' + property : path + '[' + property + ']';
+          } else {
+            return property
+          }
+        })(),
+        matchValue: (() => {
+          const currentValue = objectType === 'object' || objectType === 'array'
+                              ? JSON.stringify(object[property])
+                              : object[property];
+          const query = valueType === 'object' || valueType === 'array'
+                        ? JSON.stringify(value)
+                        : value;
 
- 	const findValue = (object, value) => {
-  	for (const key in object) {
-    	const currentValue = object[key];
-    	const isObject = typeof currentValue === 'object';
-      const isArray = Array.isArray(currentValue);
-    	const isEqual = isObject && isArray
-                      ? JSON.stringify(currentValue) === JSON.stringify(value)
-                      : currentValue === value;
-      if (!path) {
-        path = key;
-      } else {
-      	if (isObject || isEqual) {
-        	path += isNaN(key) ? '.' + key : '[' + key + ']';
-        }
+          return query === currentValue;
+        })(),
       }
 
-      if (isEqual) {
-      	found = true;
-      	break;
-      }
+      memory.push(slot);
 
-      if (isObject) {
-      	findValue(currentValue, value);
+      if ( objectType === 'object' || objectType === 'array') {
+        handler.object(object[property], slot.path);
       }
-    }
+    }),
+    array: (array, path) => array.map((object) => handler.object(object, path)),
   };
 
-  findValue(object, value);
+  handler[getType(object)](object);
 
-  let chunks = path.split('.');
+  console.log('object: ', object, 'value: ', value);
+  //console.log('memory: ', memory);
+  //console.log('match: ', memory.find((slot) => slot.matchValue));
 
-  while (chunks.length > 2) {
-  	chunks.shift();
-  }
+  const slot = memory.find((slot) => slot.matchValue);
 
-  path = chunks.join('.');
-
-  return found ? path : 'not found';
+  return slot ? slot.path : 'not found';
 }
